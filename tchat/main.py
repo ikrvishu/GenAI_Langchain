5971498d3992dd718322b098b91604df86e19ef5
Kumar Vishal <kumar.vishal1307@gmail.comgit config --global user.name ikrvishugit config --global user.email kumar.vishal1307@gmail.com>
2024-07-16 21:37:43 +0530

    Tools with multiple arguments

--- tchat/main.py
+++ tchat/main.py
@@ -9,7 +9,7 @@ from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
-
+from tools.report import write_report_tool
 
 load_dotenv()
 
@@ -18,13 +18,22 @@ chat = ChatOpenAI()
 tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
-        SystemMessage(content=f"You are an AI that has access to a SQLite database.\n{tables}"),
+        SystemMessage(content=(
+            "You are an AI that has access to a SQLite database.\n"
+            f"The database has tables of: {tables}\n"
+            "Do not make any assumptions about what tables exist "
+            "or what columns exist. Instead, use the 'describe_tables' function"
+        )),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools = [run_query_tool, describe_tables_tool]
+tools = [
+    run_query_tool,
+    describe_tables_tool,
+    write_report_tool
+]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
@@ -38,5 +47,5 @@ agent_executor = AgentExecutor(
     tools=tools
 )
 
-agent_executor("How many users have provided a shipping address?")
+agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
 # agent_executor("how many users are there?")
